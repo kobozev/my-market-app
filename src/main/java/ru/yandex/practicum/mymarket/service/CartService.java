e6af370a9fcec190f7b99ca040f8ee8e6ae9b91b
Vasiliy Kobozev <vasiliy.kobozev@gmail.com>
2026-04-08 23:19:01 +0300

    Refactor CartService using DTO records

--- src/main/java/ru/yandex/practicum/mymarket/service/CartService.java
+++ src/main/java/ru/yandex/practicum/mymarket/service/CartService.java
@@ -3,11 +3,16 @@ package ru.yandex.practicum.mymarket.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.mymarket.dto.CartDto;
+import ru.yandex.practicum.mymarket.dto.ItemDto;
+import ru.yandex.practicum.mymarket.mapper.ItemMapper;
 import ru.yandex.practicum.mymarket.model.Cart;
 import ru.yandex.practicum.mymarket.model.CartItem;
 import ru.yandex.practicum.mymarket.model.Item;
 import ru.yandex.practicum.mymarket.repository.CartRepository;
 import ru.yandex.practicum.mymarket.repository.ItemRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,32 +21,51 @@ public class CartService {
 
     private final CartRepository cartRepository;
     private final ItemRepository itemRepository;
+    private final ItemMapper mapper;
 
+    // Получить или создать корзину
     public Cart getOrCreate(Long cartId) {
         return cartRepository.findById(cartId)
                 .orElseGet(() -> cartRepository.save(new Cart()));
     }
 
+    // Получить корзину как DTO
+    public CartDto getCart(Long cartId) {
+        Cart cart = getOrCreate(cartId);
+
+        List<ItemDto> items = cart.getItems().stream()
+                .map(ci -> mapper.toDto(ci.getItem(), ci.getCount()))
+                .toList();
+
+        long total = items.stream()
+                .mapToLong(i -> i.price() * i.count())
+                .sum();
+
+        return new CartDto(items, total);
+    }
+
+    // Добавить товар
     public void add(Long cartId, Long itemId) {
         Cart cart = getOrCreate(cartId);
         Item item = itemRepository.findById(itemId).orElseThrow();
 
-        CartItem ci = cart.getItems().stream()
-                .filter(i -> i.getItem().getId().equals(itemId))
+        CartItem cartItem = cart.getItems().stream()
+                .filter(ci -> ci.getItem().getId().equals(itemId))
                 .findFirst()
                 .orElse(null);
 
-        if (ci == null) {
-            ci = new CartItem();
-            ci.setCart(cart);
-            ci.setItem(item);
-            ci.setCount(1);
-            cart.getItems().add(ci);
+        if (cartItem == null) {
+            cartItem = new CartItem();
+            cartItem.setCart(cart);
+            cartItem.setItem(item);
+            cartItem.setCount(1);
+            cart.getItems().add(cartItem);
         } else {
-            ci.setCount(ci.getCount() + 1);
+            cartItem.setCount(cartItem.getCount() + 1);
         }
     }
 
+    // Уменьшить количество
     public void minus(Long cartId, Long itemId) {
         Cart cart = getOrCreate(cartId);
 
@@ -52,5 +76,16 @@ public class CartService {
             }
             return false;
         });
+    }
+
+    // Полное удаление товара
+    public void delete(Long cartId, Long itemId) {
+        Cart cart = getOrCreate(cartId);
+        cart.getItems().removeIf(ci -> ci.getItem().getId().equals(itemId));
+    }
+
+    // Очистка корзины (для заказа)
+    public void clear(Long cartId) {
+        getOrCreate(cartId).getItems().clear();
     }
 }
