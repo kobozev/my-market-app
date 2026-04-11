4bf41a607c2c0dfda5201542b54139f59c230c0c
Vasiliy Kobozev <vasiliy.kobozev@gmail.com>
2026-04-11 20:35:32 +0300

    Add docker-compose configuration and application profiles

--- src/test/java/ru/yandex/practicum/mymarket/config/TestcontainersConfig.java
+++ src/test/java/ru/yandex/practicum/mymarket/config/TestcontainersConfig.java
@@ -4,14 +4,25 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration
 public class TestcontainersConfig {
 
-    @Bean
+/*    @Bean
     @ServiceConnection
     public PostgreSQLContainer<?> postgres() {
         return new PostgreSQLContainer<>("postgres:16-alpine")
                 .withReuse(true);
-    }
+    }*/
+
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16-alpine")
+                    .withDatabaseName("mymarket")
+                    .withUsername("mymarket")
+                    .withPassword("mymarket")
+                    .withReuse(true);
 }
